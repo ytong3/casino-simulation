c8d814cd1090c52dad326925c1358044b1400ded
Aaron Tong <ytong3@utk.edu>
2018-01-16 22:32:07 -0500

    WIP

---
+++ BinBuilder.py
@@ -0,0 +1,6 @@
+'''
+It is often helpful to create a class that is used to build an instance of another class. This is a design pattern sometimes called a Builder. We’ll design an object that builds the various Bins and assigns them to the Wheel. This will fill the need left open in the Wheel Class.
+'''
+
+class BinBuilder():
+    
--- Wheel.py
+++ Wheel.py
@@ -7,6 +7,12 @@ class Wheel():
         self.rand = random.Random()
         self.bins = tuple(Bin() for i in range(38))
 
-    def addOutcome(self, number, outcome):
-        outcome bins[number]
+    def add_outcome(self, number, outcome):
+        self.bins[number]
+
+    def next(self):
+        return self.bins[self.rand.randint(0,37)]
+
+    def get(self,bin_number):
+        return self.bins[bin_number]
        
